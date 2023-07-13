88ea73903ee62bdbec9a808e8611e4b9c1c617b7
shaneduffy91 <shaneduffy38@yahoo.ie>
2023-07-13 18:01:23 +0000

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let jamesBondFilms = ["No Time To Die", "Spectre", "Skyfall", "Quantum Of Solace", "Casino Royale", "Die Another Day", "Tomorrow Never Dies", "The World Is Not Enough ", "GoldenEye", "Licence TO Kill"]
+
+let championsLeague = ["Manchester City", "Real Madrid", "Chelsea", "Bayern Munich", "Liverpool", "Barcelona", "Inter Milan", "Manchester United", "AC Milan", "Porto"]
+
+let looneyTunes = ["Bugs Bunny", "Daaffy Duck", "Tweety", "Sylvester", "Foghorn Leghorn", "Porky Pig", "Elmer Fudd", "Yosemite Sam", "Tasmanian Devil", "Speedy Gonzales"]
+
+let hogwartsProfessors = ["Dumbledore", "McGonagall", "Snape", "Flitwick", "Sprout", "Lupin", "Grubbly-Plank", "Trelawney", "Lockhart", "Slughorn"]
+
+let citiesEU = ["Rome", "Bari", "Riga", "Bath", "Pisa", "Bonn", "Cork", "Vigo", "Lyon", "Nice" ]
