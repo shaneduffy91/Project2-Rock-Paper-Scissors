542fe4aea793fcc25b1844f8ee872213ca38a91e
shaneduffy91 <shaneduffy38@yahoo.ie>
2023-07-14 13:08:29 +0000

    Added images and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,79 @@
-let jamesBondFilms = ["No Time To Die", "Spectre", "Skyfall", "Quantum Of Solace", "Casino Royale", "Die Another Day", "Tomorrow Never Dies", "The World Is Not Enough ", "GoldenEye", "Licence TO Kill"]
+/*
+1. When the player comes to the page, they see 3 buttons: rock, paper, scissors
+2. When the player clicks on one of the buttons, the game begins, the computer
+    randomly chooses rock, paper or scissors, and the player's and computer's 
+    choices are compared to see who wins.
 
-let championsLeague = ["Manchester City", "Real Madrid", "Chelsea", "Bayern Munich", "Liverpool", "Barcelona", "Inter Milan", "Manchester United", "AC Milan", "Porto"]
+3. The outcome of the turn is displayed on the screen.
 
-let looneyTunes = ["Bugs Bunny", "Daaffy Duck", "Tweety", "Sylvester", "Foghorn Leghorn", "Porky Pig", "Elmer Fudd", "Yosemite Sam", "Tasmanian Devil", "Speedy Gonzales"]
+4. Next turn begins (1)
+5. The game ends when either of the players reaches 5 points.
+   - Simpler version -- the game never ends.
+*/
 
-let hogwartsProfessors = ["Dumbledore", "McGonagall", "Snape", "Flitwick", "Sprout", "Lupin", "Grubbly-Plank", "Trelawney", "Lockhart", "Slughorn"]
 
-let citiesEU = ["Rome", "Bari", "Riga", "Bath", "Pisa", "Bonn", "Cork", "Vigo", "Lyon", "Nice" ]
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
+
+const turnOutcomeDiv = document.getElementById('turn-outcome');
+
+rockButton.addEventListener('click', playTurn)
+paperButton.addEventListener('click', playTurn)
+scissorsButton.addEventListener('click', playTurn)
+
+
+function playTurn(event){
+   let clickedButton = event.currentTarget
+
+   let playerCharacter = clickedButton.id
+
+   let computerCharacter = getComputerCharacter()
+
+   // compare player/computer character and print the outcome
+   figureOutAndDisplayTurnWinner(playerCharacter, computerCharacter)
+}
+
+function figureOutAndDisplayTurnWinner(playerCharacter, computerCharacter) {
+
+    if (playerCharacter === computerCharacter) {
+        // draw
+        turnOutcomeDiv.innerText = 'Draw'
+    }
+    else if (playerCharacter === 'rock' && computerCharacter === 'scissors') {
+        // player wins
+        turnOutcomeDiv.innerText = 'You win'
+    }
+    else if (playerCharacter === 'paper' && computerCharacter === 'rock') {
+        // player wins
+        turnOutcomeDiv.innerText = 'You win'
+    }
+    else if (playerCharacter === 'scissors' && computerCharacter === 'paper') {
+        // player wins
+        turnOutcomeDiv.innerText = 'You win'
+    }
+    else {
+        // computer wins
+        turnOutcomeDiv.innerText = 'Computer wins'
+    }
+}
+
+
+function getComputerCharacter() {
+
+    let characterOptions = ['rock', 'paper', 'scissors']
+
+    let randomCharacterIndex = Math.floor(Math.random() * characterOptions.length)
+
+    let randomCharacter = characterOptions[randomCharacterIndex]
+
+    console.log(randomCharacter)
+    return randomCharacter
+}
+
+function incrementScore() {
+    
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+}
+
